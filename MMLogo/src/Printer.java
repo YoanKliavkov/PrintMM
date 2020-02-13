79e01039034e3f6d644927f224a5e7cd1f70998e
YoanKliavkov <60933007+YoanKliavkov@users.noreply.github.com>
2020-02-13 20:39:27 +0200

    Update Printer.java

--- MMLogo/src/Printer.java
+++ MMLogo/src/Printer.java
@@ -1,56 +1,60 @@
 public class Printer {
 	public static void main(String[] args) {
-		printMM(5);
+		printMM(3);
 	}
 
 	/**
-	 * Методът printММ приема широчината на буквите и принтира MM в конзолата.
-	 * Време за изпълнеие: O(width^2)
-	 * Памет: O(1)
+	 * Методът printММ приема широчината на бурвите и принтира MM в конзолата. Време
+	 * за изпълнеие: O(width^2) Памет: O(1)
 	 * 
 	 * @param width - широчината на буквите
 	 */
 	public static void printMM(final int width) {
 
 		// Всяко от двете М-та (те са еднакви) се състоят от width + 1 реда и width * 5
-		// колони
-		// Тоест двете М-та имат общо width * 10 колони
+		// колони.
+		// Тоест двете М-та имат общо width * 10 колони.
 
 		final int rows = width + 1;
 		final int cols = width * 5;
 
+		// Правим малко изчисления преди цикъла, за да не се изпълняват напразно
+		// многократно.
+		final int width2 = width * 2;
+		final int width3 = width * 3;
+		final int width4 = width * 4;
+
 		for (int x = 0; x < rows; x++) {
 
-			// принтираме реда два пъти, защото имаме 2 идентични М-та
+			// Принтираме реда два пъти, защото имаме 2 идентични М-та.
 			for (int p = 0; p < 2; p++) {
 				for (int y = 0; y < cols; y++) {
+
+					// За да начертая буквата М съставям алгоритъм, в който задавам звездите на
+					// нулев ред и в следващите редове правя диагонални проекции на звездите от
+					// нулевия ред.
+
+					// Всички елементи на 0 ред с номер на колоната принадлежащ на [width, width*2)
+					// или [width*3, width*4) са '*'.
+
+					// За следващите редове проверяваме дали y - x или y + x попада в поне един от
+					// интервалите [width,
+					// width*2) или [width*3 , width*4).
 
 					final int leftDiag = y - x;
 					final int rightDiag = y + x;
 
-					// За да разберем дали трябва да принтираме '-' или '*' прекарваме мисловен
-					// диагонал
-					// от вмоментащната точка с координати (x, y) към ред 0. Получават се две точки
-					// (0, y - x) и (0, y + x).
-					// Ако поне една от тези точки съответства на '*', то принтираме '*', в противен
-					// случаи принтираме -.
-					// Ясно се вижда че точките на ред 0 отговарящо на '*' са в колоните [width,
-					// 2*width) и [3*width, 4*width).
-
-					if ((leftDiag >= width && leftDiag < 2 * width) || (rightDiag >= 3 * width && rightDiag < 4 * width)
-							|| (leftDiag >= 3 * width && leftDiag < 4 * width)
-							|| (rightDiag >= width && rightDiag < 2 * width)) {
+					if ((leftDiag >= width && leftDiag < width2) || (rightDiag >= width3 && rightDiag < width4)
+							|| (leftDiag >= width3 && leftDiag < width4)
+							|| (rightDiag >= width && rightDiag < width2)) {
 						System.out.print("*");
 					} else {
 						System.out.print("-");
 					}
 				}
 			}
-			
-			//Нов ред
+			// Нов ред
 			System.out.println();
 		}
-
 	}
-
 }
